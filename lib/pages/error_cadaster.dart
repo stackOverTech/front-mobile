103e8347ae2bcf2fc32cb81da43b42c11c823226
Isabela Freitas Franco <francofreitasisabela@gmail.com>
2024-09-25 20:22:52 -0300

    fixing logical problems

--- lib/pages/error_cadaster.dart
+++ lib/pages/error_cadaster.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:one/pages/cadaster.dart';
-import 'dart:async';
 
 class ErrorScreen extends StatefulWidget {
   @override
@@ -8,31 +7,21 @@ class ErrorScreen extends StatefulWidget {
 }
 
 class _ErrorScreenState extends State<ErrorScreen> {
-  bool _isMessageVisible = false;
-
   @override
   void initState() {
     super.initState();
-    _simulateDatabaseCall();
+    _navigateToCadasterAfterDelay();
   }
 
-  Future<void> _simulateDatabaseCall() async {
-    await Future.delayed(Duration(seconds: 1));
-    int status = 200;
-
-    if (status == 200) {
-      setState(() {
-        _isMessageVisible = true;
-      });
-
-      await Future.delayed(Duration(seconds: 5));
+  void _navigateToCadasterAfterDelay() {
+    Future.delayed(Duration(seconds: 5), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => CadasterPage()),
         );
       }
-    }
+    });
   }
 
   @override
@@ -40,23 +29,23 @@ class _ErrorScreenState extends State<ErrorScreen> {
     return Scaffold(
       backgroundColor: Color(0xFFF4F4F4),
       body: Center(
-        child: _isMessageVisible
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      child: Image.asset(
-                        'android/app/src/main/res/drawable/error.png',
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                  Text('Erro ao cadastrar', style: TextStyle(fontSize: 24, color: Color(0xFF333333))
-                  ),
-                ],
-              )
-            : Container(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Image.asset(
+                'android/app/src/main/res/drawable/error.png',
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Text(
+              'Erro ao cadastrar',
+              style: TextStyle(fontSize: 24, color: Color(0xFF333333)),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/sucess_cadaster.dart
+++ lib/pages/sucess_cadaster.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:one/pages/introduction.dart';
-import 'dart:async';
 
 class NotificationScreen extends StatefulWidget {
   @override
@@ -8,31 +7,21 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-  bool _isMessageVisible = false;
-
   @override
   void initState() {
     super.initState();
-    _simulateDatabaseCall();
+    _navigateToIntroductionAfterDelay();
   }
 
-  Future<void> _simulateDatabaseCall() async {
-    await Future.delayed(Duration(seconds: 1));
-    int status = 200;
-
-    if (status == 200) {
-      setState(() {
-        _isMessageVisible = true;
-      });
-
-      await Future.delayed(Duration(seconds: 5));
+  void _navigateToIntroductionAfterDelay() {
+    Future.delayed(Duration(seconds: 5), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => IntroductionPage()),
         );
       }
-    }
+    });
   }
 
   @override
@@ -40,23 +29,23 @@ class _NotificationScreenState extends State<NotificationScreen> {
     return Scaffold(
       backgroundColor: Color(0xFFF4F4F4),
       body: Center(
-        child: _isMessageVisible
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      child: Image.asset(
-                        'android/app/src/main/res/drawable/task_alt.png',
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                  Text('Cadastrado com sucesso!', style: TextStyle(fontSize: 24, color: Color(0xFF333333))
-                  ),
-                ],
-              )
-            : Container(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Image.asset(
+                'android/app/src/main/res/drawable/task_alt.png',
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Text(
+              'Cadastrado com sucesso!',
+              style: TextStyle(fontSize: 24, color: Color(0xFF333333)),
+            ),
+          ],
+        ),
       ),
     );
   }
