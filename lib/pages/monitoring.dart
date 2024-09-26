7b31b529b1dcb918f4084c22c04998696a414647
Beatriz Bortolucci <beatriz.domingos@germinare.org.br>
2024-09-26 09:15:06 -0300

    atualizando tela de monitorias

--- lib/pages/monitoring.dart
+++ lib/pages/monitoring.dart
@@ -6,22 +6,16 @@ class MonitoringPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Cabeçalho com o nome do usuário e saudação
           Container(
-            //  decoration: BoxDecoration(
-            //   borderRadius: BorderRadius.circular(20),
-            //   color: const Color.fromRGBO(61, 112, 128, 1),
-            //  ),
-            padding: const EdgeInsets.all(16.0),
-            color: const Color.fromRGBO(61, 112, 128, 1),
+            padding: const EdgeInsets.all(0.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 AppBar(
-                  automaticallyImplyLeading: false, 
+                  automaticallyImplyLeading: false,
                   backgroundColor: const Color.fromRGBO(61, 112, 128, 1),
-                  elevation: 0,
                   title: const Text(
                     'One',
                     style: TextStyle(
@@ -40,103 +34,125 @@ class MonitoringPage extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(height: 14),
-          Text(
-            'Taylor, sua \nmonitoria te \nespera :)',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xFF2C313A),
-            ),
-          ),
-          // Disciplinas existentes
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Disciplinas Existentes',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(253, 253, 253, 1.0),
+                borderRadius: const BorderRadius.only(
+                  topLeft:
+                      Radius.circular(20), 
+                  topRight:
+                      Radius.circular(20), 
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromRGBO(61, 112, 128, 1),
+                    spreadRadius: 12,
+                    offset: Offset(0, 3),
                   ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SubjectCard(
-                      title: 'Matemática',
-                      color: Colors.redAccent,
+                ],
+              ),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Taylor, sua \nmonitoria te \nespera :)',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF2C313A),
                     ),
-                    SubjectCard(
-                      title: 'História',
-                      color: Colors.purpleAccent,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          // Disciplinas que recebe monitoria
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Disciplinas que recebo monitoria',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
                   ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SubjectCard(
-                      title: 'DAD',
-                      color: Colors.blueAccent,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Disciplinas Existentes',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SubjectCard(
+                              title: 'Matemática',
+                              color: const Color(0xFFBB4C53),
+                            ),
+                            SubjectCard(
+                              title: 'História',
+                              color: const Color(0xFF7E4987),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    SubjectCard(
-                      title: 'Português',
-                      color: Colors.orangeAccent,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Disciplinas que recebo monitoria',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SubjectCard(
+                              title: 'DAD',
+                              color: const Color(0xFF305A77),
+                            ),
+                            SubjectCard(
+                              title: 'Português',
+                              color: const Color(0xFFD27051),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 1,
-          onTap: (int index) {
-            if (index == 0) {
-              // Verifica se o item "home" foi clicado
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => HomePage(),
-                ),
-              );
-            }
-          },
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.book), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.people), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
-          ],
-          selectedItemColor: const Color.fromRGBO(61, 112, 128, 1),
-          unselectedItemColor: Colors.white,
-          backgroundColor: const Color.fromRGBO(72, 79, 92, 1.0),
-          elevation: 10,
-          selectedIconTheme: const IconThemeData(size: 24, weight: 24),
-          unselectedIconTheme: const IconThemeData(size: 24, weight: 24),
-          type: BottomNavigationBarType.fixed),
+        currentIndex: 1,
+        onTap: (int index) {
+          if (index == 0) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+        ],
+        selectedItemColor: const Color.fromRGBO(61, 112, 128, 1),
+        unselectedItemColor: Colors.white,
+        backgroundColor: const Color.fromRGBO(72, 79, 92, 1.0),
+        elevation: 10,
+        selectedIconTheme: const IconThemeData(size: 24, weight: 24),
+        unselectedIconTheme: const IconThemeData(size: 24, weight: 24),
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
